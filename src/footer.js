d705c0b51089cf277da8ca8ead35d75ac4bf6132
marcosdellavecchia <33458523+marcosdellavecchia@users.noreply.github.com>
2020-08-31 19:59:05 -0300

    Se agrego modal en el footer que muestra información acerca del proyecto

--- src/footer.js
+++ src/footer.js
@@ -1,71 +1,95 @@
 import React from "react";
+import { Modal, ModalHeader, ModalBody, ModalFooter } from "reactstrap";
 import "./footer.css";
 
-const Footer = () => {
-  return (
-    <div className="footer-box">
-      <div className="container footer-data">
-        <div className="footer-item">
-          <h6>CRYPTOBOARD PROJECT</h6>
-          <a href="https://cryptoboard-project.web.app/">Acerca del proyecto</a>
-          <a
-            href="https://github.com/marcosdellavecchia/crypto-dashboard"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            {" "}
-            Código fuente
-          </a>
+class Footer extends React.Component {
+  state = { isModalOpen: false };
+  openModal = () => {
+    this.setState({ isModalOpen: !this.state.isModalOpen });
+  };
 
-          <a
-            href="https://www.coingecko.com/api/documentations/v3"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            CoinGecko API
-          </a>
-        </div>
+  render() {
+    return (
+      <div className="footer-box">
+        <div className="container footer-data">
+          <div className="footer-item">
+            <h6>CRYPTOBOARD PROJECT</h6>
 
-        <div className="footer-item">
-          <h6>REDES SOCIALES</h6>
-          <div className="social">
+            {/* Modal que abre la información del proyecto */}
+            <span className="about" onClick={this.openModal}>
+              Acerca del proyecto
+            </span>
+            <Modal isOpen={this.state.isModalOpen}>
+              <ModalHeader>Acerca del proyecto</ModalHeader>
+              <ModalBody>Este es es el body</ModalBody>
+              <ModalFooter>
+                <button
+                  className="btn-primary modal-return"
+                  onClick={this.openModal}
+                >
+                  Volver al sitio
+                </button>
+              </ModalFooter>
+            </Modal>
+
             <a
-              href="https://twitter.com/marcosdv"
+              href="https://github.com/marcosdellavecchia/crypto-dashboard"
               target="_blank"
               rel="noopener noreferrer"
             >
-              <i className="fa fa-twitter fa-lg" aria-hidden="true"></i>
+              {" "}
+              Código fuente
             </a>
             <a
-              href="https://www.linkedin.com/in/marcos-dv/"
+              href="https://www.coingecko.com/api/documentations/v3"
               target="_blank"
               rel="noopener noreferrer"
             >
-              <i className="fa fa-linkedin fa-lg" aria-hidden="true"></i>
-            </a>
-            <a
-              href="https://github.com/marcosdellavecchia"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <i className="fa fa-github fa-lg" aria-hidden="true"></i>
+              CoinGecko API
             </a>
           </div>
-        </div>
 
-        <div className="footer-item">
-          <h6>CRÉDITOS</h6>
-          <p>
-            Hecho con{" "}
-            <span role="img" aria-hidden="true">
-              💙
-            </span>{" "}
-            por Marcos DV
-          </p>
+          <div className="footer-item">
+            <h6>REDES SOCIALES</h6>
+            <div className="social">
+              <a
+                href="https://twitter.com/marcosdv"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <i className="fa fa-twitter fa-lg" aria-hidden="true"></i>
+              </a>
+              <a
+                href="https://www.linkedin.com/in/marcos-dv/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <i className="fa fa-linkedin fa-lg" aria-hidden="true"></i>
+              </a>
+              <a
+                href="https://github.com/marcosdellavecchia"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <i className="fa fa-github fa-lg" aria-hidden="true"></i>
+              </a>
+            </div>
+          </div>
+
+          <div className="footer-item">
+            <h6>CRÉDITOS</h6>
+            <p>
+              Hecho con{" "}
+              <span role="img" aria-hidden="true">
+                💙
+              </span>{" "}
+              por Marcos DV
+            </p>
+          </div>
         </div>
       </div>
-    </div>
-  );
-};
+    );
+  }
+}
 
 export default Footer;
